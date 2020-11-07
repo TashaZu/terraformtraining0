de94719e67d50a4520f4cae63f9eff0f866dc0e2
Tasha zhu <tashazhu@Tashas-MacBook-Pro.local>
2020-11-07 13:28:51 -0500

    acg

--- prod.tf
+++ prod.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   profile = "default"
   region = "us-west-2"
-  }
+}
 
 resource "aws_s3_bucket" "prod-tf-course" {
   bucket = "tfs3-tasha-20201103"
@@ -9,6 +9,20 @@ resource "aws_s3_bucket" "prod-tf-course" {
 }
 
 resource "aws_default_vpc" "default"{
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
 }
 
 resource "aws_security_group" "prod_web" {
@@ -38,26 +52,57 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true"
   }
 }
-resource "aws_instance" "prod_web" {
-  count = 2
-  ami = "ami-019c091d13a1fa156"
-  instance_type = "t2.nano"
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-    ]
 
-    tags = {
-    "Terraform" : "true"
+
+
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+ 
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
   }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
 }
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix = "prod-web"
+  image_id = "ami-019c091d13a1fa156"
+  instance_type =  "t2.micro"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+//  availability_zones = ["us-west-2a","us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  desired_capacity = 1
+  max_size = 1
+  min_size = 1
+
+  launch_template {
+    id = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key = "Terraform" 
+    value =  "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb = aws_elb.prod_web.id
+}
+
